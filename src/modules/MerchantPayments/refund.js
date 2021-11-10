631cf1dfa5769a95f390b5dd15f6bdca50e8d0e4
Rohith C M <rohith.cm@fingent.com>
2021-11-10 13:46:34 +0530

    Refund bug fix

--- src/modules/MerchantPayments/refund.js
+++ src/modules/MerchantPayments/refund.js
@@ -1,16 +1,17 @@
-import requestMaker from '../../utils/requestMaker';
-import checkRequiredProps from '../../utils/checkRequiredKeys';
-import { transactionObjectValidtor } from '../../utils/transactionObjectValidator';
+import requestMaker from "../../utils/requestMaker";
+import checkRequiredProps from "../../utils/checkRequiredKeys";
+import { transactionObjectValidtor } from "../../utils/transactionObjectValidator";
 export default (props, onError) => {
-    if (
-        checkRequiredProps(props, ['data', 'callbackUrl'], onError) &&
-        checkRequiredProps(data, ['amount', 'currency'], onError) &&
-        transactionObjectValidtor(data, ['amount'], onError)
-    ) {
-        let { data, correlationId = '', callbackUrl } = props;
-        return requestMaker(`/transactions/type/adjustment`, {
-            'X-CorrelationID': correlationId,
-            'X-Callback-URL': callbackUrl,
-        }).post(data);
-    }
+  const { data } = props;
+  if (
+    checkRequiredProps(props, ["data", "callbackUrl"], onError) &&
+    checkRequiredProps(data, ["amount", "currency"], onError) &&
+    transactionObjectValidtor(data, ["amount"], onError)
+  ) {
+    let { data, correlationId = "", callbackUrl } = props;
+    return requestMaker(`/transactions/type/adjustment`, {
+      "X-CorrelationID": correlationId,
+      "X-Callback-URL": callbackUrl,
+    }).post(data);
+  }
 };
