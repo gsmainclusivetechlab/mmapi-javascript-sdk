bd3130838ec6a87c070087a4fc246bbf68e19c11
sebinFingent <sebin.jacob@fingent.com>
2021-11-11 11:39:16 +0530

    added readme

--- public/basiAuthInterTransfers.js
+++ public/basiAuthInterTransfers.js
@@ -5,82 +5,84 @@ gsma.initBasicAuth(
     
 );
 GSMA_BASIC_AUTH = gsma.auth;
+function requestQuotation(){
+    GSMA_BASIC_AUTH.international({
+        type:'requestQuotation',
+        data:{
+            "creditParty": [
+                {
+                    "key": "accountid",
+                    "value": "2000"
+                }
+            ],
+            "debitParty": [
+                {
+                    "key": "accountid",
+                    "value": "2999"
+                }
+            ],
+            "requestAmount": "75.30",
+            "requestCurrency": "RWF",
+            "requestDate": "2018-07-03T11:43:27.405Z",
+            "type": "inttransfer",
+            "subType": "abc",
+            "chosenDeliveryMethod": "agent",
+            "senderKyc": {
+                "nationality": "GB",
+                "dateOfBirth": "1970-07-03T11:43:27.405Z",
+                "occupation": "Manager",
+                "employerName": "MFX",
+                "contactPhone": "+447125588999",
+                "gender": "m",
+                "emailAddress": "luke.skywalkeraaabbb@gmail.com",
+                "birthCountry": "GB",
+                "idDocument": [
+                    {
+                        "idType": "nationalidcard",
+                        "idNumber": "1234567",
+                        "issueDate": "2018-07-03T11:43:27.405Z",
+                        "expiryDate": "2021-07-03T11:43:27.405Z",
+                        "issuer": "UKPA",
+                        "issuerPlace": "GB",
+                        "issuerCountry": "GB",
+                        "otherIdDescription": "test"
+                    }
+                ],
+                "postalAddress": {
+                    "country": "GB",
+                    "addressLine1": "111 ABC Street",
+                    "city": "New York",
+                    "stateProvince": "New York",
+                    "postalCode": "ABCD"
+                },
+                "subjectName": {
+                    "title": "Mr",
+                    "firstName": "Luke",
+                    "middleName": "R",
+                    "lastName": "Skywalker",
+                    "fullName": "Luke R Skywalker",
+                    "nativeName": "ABC"
+                }        
+            },    
+            "customData": [
+                {
+                "key": "keytest",
+                "value": "keyvalue"
+                }
+            ],    
+          "sendingServiceProviderCountry": "AD",
+          "originCountry": "AD",
+          "receivingCountry": "AD"
+        },
+        onSuccess: (data, status) => {
+            console.log('BASIC AUTH success requestQuotation', status, data);
+        },
+        onFailure: (error, status) => {
+            console.error('BASIC AUTH error requestQuotation', error);
+        },
+    })
+}
 
-// GSMA_BASIC_AUTH.international({
-//     type:'requestQuotation',
-//     data:{
-//         "creditParty": [
-//             {
-//                 "key": "accountid",
-//                 "value": "2000"
-//             }
-//         ],
-//         "debitParty": [
-//             {
-//                 "key": "accountid",
-//                 "value": "2999"
-//             }
-//         ],
-//         "requestAmount": "75.30",
-//         "requestCurrency": "RWF",
-//         "requestDate": "2018-07-03T11:43:27.405Z",
-//         "type": "inttransfer",
-//         "subType": "abc",
-//         "chosenDeliveryMethod": "agent",
-//         "senderKyc": {
-//             "nationality": "GB",
-//             "dateOfBirth": "1970-07-03T11:43:27.405Z",
-//             "occupation": "Manager",
-//             "employerName": "MFX",
-//             "contactPhone": "+447125588999",
-//             "gender": "m",
-//             "emailAddress": "luke.skywalkeraaabbb@gmail.com",
-//             "birthCountry": "GB",
-//             "idDocument": [
-//                 {
-//                     "idType": "nationalidcard",
-//                     "idNumber": "1234567",
-//                     "issueDate": "2018-07-03T11:43:27.405Z",
-//                     "expiryDate": "2021-07-03T11:43:27.405Z",
-//                     "issuer": "UKPA",
-//                     "issuerPlace": "GB",
-//                     "issuerCountry": "GB",
-//                     "otherIdDescription": "test"
-//                 }
-//             ],
-//             "postalAddress": {
-//                 "country": "GB",
-//                 "addressLine1": "111 ABC Street",
-//                 "city": "New York",
-//                 "stateProvince": "New York",
-//                 "postalCode": "ABCD"
-//             },
-//             "subjectName": {
-//                 "title": "Mr",
-//                 "firstName": "Luke",
-//                 "middleName": "R",
-//                 "lastName": "Skywalker",
-//                 "fullName": "Luke R Skywalker",
-//                 "nativeName": "ABC"
-//             }        
-//         },    
-//         "customData": [
-//             {
-//             "key": "keytest",
-//             "value": "keyvalue"
-//             }
-//         ],    
-//       "sendingServiceProviderCountry": "AD",
-//       "originCountry": "AD",
-//       "receivingCountry": "AD"
-//     },
-//     onSuccess: (data, status) => {
-//         console.log('BASIC AUTH success requestQuotation', status, data);
-//     },
-//     onFailure: (error, status) => {
-//         console.error('BASIC AUTH error requestQuotation', error);
-//     },
-// })
 
 GSMA_BASIC_AUTH.international({
     type:'transfer',
@@ -101,7 +103,7 @@ GSMA_BASIC_AUTH.international({
         ],
         "internationalTransferInformation": {
             "originCountry": "GB",
-            "quotationReference": "REF-1636532771223",
+            // "quotationReference": "REF-1636537211456",
             // "quoteId": "{{quoteId}}",
             "receivingCountry": "RW",
             "remittancePurpose": "personal",
@@ -151,10 +153,10 @@ GSMA_BASIC_AUTH.international({
         "requestingOrganisationIdentifier": "testorganisation"
       }
     },
-    onSuccess: (data, status) => {
+    onSuccess: (data,header, status) => {
         console.log('BASIC AUTH success international transfer', status, data);
     },
-    onFailure: (error, status) => {
+    onFailure: (error,header, status) => {
         console.error('BASIC AUTH error international transfer', error);
     },
 })
