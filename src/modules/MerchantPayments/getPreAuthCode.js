8b0a22f5cb0ae7cdf0a1a34847221193a7ab498d
sebinFingent <sebin.jacob@fingent.com>
2021-11-16 15:20:47 +0530

    added new req to get pre auth code

--- src/modules/MerchantPayments/getPreAuthCode.js
+++ src/modules/MerchantPayments/getPreAuthCode.js
@@ -5,24 +5,18 @@ import checkRequiredProps from '../../utils/checkRequiredKeys';
  * @param  {} props must contain account identifier deatails and page offset & limit
  * @param  {} onError triggers some validation errors
  */
-export default function getAllTransactions(props, onError) {
+export default function getPreAuthCode(props, onError) {
     if (
         checkRequiredProps(
             props,
-            ['identifierType', 'identifier'],
+            ['identifierType', 'identifier','authorisationCode'],
             onError
         )
     ) {
-        const { identifierType, identifier, offset=null, limit=null } = props;
-        let params = null
-        if(offset && limit){
-            params = {
-                offset,
-                limit,
-            }
-        }
+        const { identifierType, identifier,authorisationCode } = props;
+      
         return requestMaker(
-            `/accounts/${identifierType}/${identifier}/transactions`
-        ).get(params);
+            `/accounts/${identifierType}/${identifier}/authorisationcodes/${authorisationCode}`
+        ).get();
     }
 }
