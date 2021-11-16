c1b0424634c3fc48bc6007f026d53138e0d56ff0
sebinFingent <sebin.jacob@fingent.com>
2021-11-16 13:21:08 +0530

    renamed functon names and added viewQuotations for polling

--- src/const/paymentTypes.js
+++ src/const/paymentTypes.js
@@ -1,5 +1,3 @@
-
-
 // common -  types
 export const BALANCE_CHECK = 'viewAccountBalance';
 export const SERVICE_AVAILABILITY = 'viewServiceAvailability';
@@ -8,6 +6,8 @@ export const REQUEST_STATE = 'viewRequestState';
 export const TRANSACTION_REFERENCE = 'viewTransaction';
 export const REVERSAL = 'createReversal';
 export const TRANSACTIONS = 'viewAccountTransaction';
+export const QUOTATION_REQ = 'createQuotation'
+export const QUOTATION_VIEW = 'viewQuotation'
 
 // merchant - types
 export const MERCHANT_INIT_PAYMENT = 'createMerchantTransaction';
@@ -22,3 +22,6 @@ export const DISBURSEMENT_BULK_STATUS = 'viewBatchTransaction';
 export const DISBURSEMENT_BULK_COMPLETED = 'viewBatchCompletions';
 export const DISBURSEMENT_BULK_REJECTED = 'viewBatchRejections';
 export const DISBURSEMENT_BULK_APPROVAL = 'updateBatchTransaction';
+
+// International 
+export const INTERNATIONAL_TRANSFER = 'createInternationalTransaction'
