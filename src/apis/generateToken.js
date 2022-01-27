569d2e06411f642f8e810c57716d804204040d18
sebinFingent <sebin.jacob@fingent.com>
2022-01-27 19:58:57 +0530

    removed failed vulnerability 3

--- src/apis/generateToken.js
+++ src/apis/generateToken.js
@@ -24,7 +24,7 @@ export default ({ username, pass, onSuccess, onFailure }) => {
         })
         .catch((error) => {
             if (error && error.response) {
-                onFailure((error.response.data, error.response.status));
+                onFailure(error.response.data, error.response.status);
             } else {
                 onFailure(error);
             }
